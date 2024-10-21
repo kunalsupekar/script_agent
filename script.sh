f805603762c4ecfd6ef4ee0de409af33cca89a3c
kunalsupekar <118017473+kunalsupekar@users.noreply.github.com>
2024-10-21 09:24:53 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -11,7 +11,7 @@ tar zxvf vsts-agent-linux-x64-3.240.1.tar.gz
 
 
 ORG_URL="https://dev.azure.com/Intern-organisation"  # Your organization URL
-PAT="flwl5rv3whnptnfy63hzrn44im7iyav6mf5pare3pxplmzwywsqa"  # Your Personal Access Token
+PAT="owz244gomhptnpzhu32hhxzfcrkld5lg3o3znj4j4ks4aqfhhkqa"  # Your Personal Access Token
 AGENT_POOL_NAME="AWS-Agent"  # The agent pool you want to add the agent to
 AGENT_NAME=$(hostname)  # Unique name for the agent
 WORK_DIR="_work"
